42870d38601cf32244c1a8e1019a2e80412d3612
sanish koju <sanishkoju949@gmail.com>
2022-12-29 15:05:55 +0545

    commit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 var sideBar=document.getElementById("sidemenu");
 var linee=document.getElementById("line")
-
+var educationn=document.querySelector("#educations")
+var skilll=document.querySelector("#skills")
 function openmenu(){
     sideBar.style.left="0px";
     linee.style.display="none"
@@ -14,4 +15,12 @@ function openmenu(){
 function closemenu(){
     sideBar.style.left="-250px";
     linee.style.display="inline"
+}
+
+function opentab(){
+    educationn.style.display="inline";
+}
+function opentabs(){
+    skilll.style.display="inline";
+
 }
