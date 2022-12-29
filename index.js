93251e612ee7004a80714edf7db8e3e583a7ff64
sanish koju <sanishkoju949@gmail.com>
2022-12-29 13:53:05 +0545

    commit

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
 
 
 var sideBar=document.getElementById("sidemenu");
+var linee=document.getElementById("line")
 
 function openmenu(){
     sideBar.style.left="0px";
+    linee.style.display="none"
+    
+    
 }
 function closemenu(){
     sideBar.style.left="-250px";
+    linee.style.display="inline"
 }
