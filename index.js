e7ddfcd589163efee77d73d2645e58884dc2d891
sanish koju <sanishkoju949@gmail.com>
2023-01-04 14:30:53 +0545

    commit

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ var moons=document.getElementById("moon");
 
 
 
+
 var bright1=document.querySelector(".navb")
 var bright2=document.querySelector(".container")
 var bright3=document.querySelector(".my-work")
@@ -88,19 +89,98 @@ function endtab(){
 
 
 
+var day= new Date();
+var hr= day.getHours();
+if(hr>=6 && hr<12){
+    bright1.style.color="black";
+    bright1.style.background="white";
 
+    bright2.style.color="black";
+    bright2.style.background="white";
+    
+    bright3.style.color="black";
+    bright3.style.background="white";
+
+    bright4.style.color="black";
+    bright4.style.background="white";
+
+    bright5.style.color="black";
+    bright5.style.background="white";
+
+    bright6.style.color="black";
+    bright6.style.background="white";
+
+       moons.style.display="inline";
+    suns.style.display="none";
+    
+    document.getElementById("pg").innerText="Good Morning !";
+
+}
+if(hr>=12 && hr<=17){
+    bright1.style.color="black";
+    bright1.style.background="white";
+
+    bright2.style.color="black";
+    bright2.style.background="white";
+    
+    bright3.style.color="black";
+    bright3.style.background="white";
+
+    bright4.style.color="black";
+    bright4.style.background="white";
+
+    bright5.style.color="black";
+    bright5.style.background="white";
+
+    bright6.style.color="black";
+    bright6.style.background="white";
+
+       moons.style.display="inline";
+    suns.style.display="none";
+    
+    document.getElementById("pg").innerText="Good Afternoon !";
+
+}
+else if(hr>=18 && hr<=0){
+bright1.style.color="white";
+    bright1.style.background="black";
+
+    bright2.style.color="white";
+    bright2.style.background="black";
+    
+    bright3.style.color="white";
+    bright3.style.background="black";
+
+    bright4.style.color="white";
+    bright4.style.background="black";
+    
+    bright5.style.color="white";
+    bright5.style.background="black";
+
+    bright6.style.color="white";
+    bright6.style.background="black";
+
+       moons.style.display="none";
+    suns.style.display="inline";
+    document.getElementById("pg").innerText="Good Night !";
+    
+
+}
 
 function risetab(){
     moons.style.display="inline";
     suns.style.display="none";
     
+    document.getElementById("pg").innerText="Dark Mode off";
+    
 
  /*
-modes.forEach((e)=>{
+ var array1=['bight1','bight2','bight3','bight4','bight5','bight6']
+array1.forEach((e)=>{
 e.style.color='black';
 });
 
-modes.forEach((e)=>{
+array1.forEach((e)=>{
     e.style.background='white';
     });
     */
@@ -134,6 +214,8 @@ modes.forEach((e)=>{
 function settab(){
     moons.style.display="none";
     suns.style.display="inline";
+    
+    document.getElementById("pg").innerText="Dark Mode";
 
     
    bright1.style.color="white";
